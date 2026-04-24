9c36df40ffb4d4d3fda59330c363928077613a7a
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-24 22:45:58 +0200

    Update RegistroVo.py

--- src/modelo/vo/RegistroVo.py
+++ src/modelo/vo/RegistroVo.py
@@ -1,7 +1,14 @@
-class RegistroVO:
-    def __init__(self, nombre, apellidos, correo, contrasena, confirmar_contrasena):
-        self.__nombre = nombre
-        self.__apellidos = apellidos
-        self.__correo = correo
-        self.__contrasena = contrasena
-        self.__confirmar_contrasena = confirmar_contrasena
+def __init__(self, nombre, apellidos, correo, contrasena):                              
+    self._nombre = nombre                                                               
+    self._apellidos = apellidos                                                         
+    self._correo = correo                                                               
+    self._contrasena = contrasena
+    
+@property                                                                               
+def nombre(self): return self._nombre                                                   
+@property                                                                               
+def apellidos(self): return self._apellidos                                             
+@property                                                                               
+def correo(self): return self._correo                                                   
+@property                                                                               
+def contrasena(self): return self._contrasena
