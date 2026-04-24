01002a6f7e276e49eca46a28cc0268798bd4278b
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-24 15:51:23 +0200

    Update UserDaoJDBC.py

--- src/modelo/dao/UserDaoJDBC.py
+++ src/modelo/dao/UserDaoJDBC.py
@@ -2,41 +2,33 @@ from src.modelo.conexion.Conexion import Conexion
 from src.modelo.vo.UsuariosVO import UsuariosVO
 
 class UserDaoJBDC(Conexion):
-    SQL_SELECT = "SELECT nombre, primer_apellido, segundo_apellido, email FROM Usuarios"
-    SQL_INSERT = ""
-    SQL_CHECK_LOGIN = "SELECT nombre, primer_apellido, segundo_apellido, email FROM Usuarios WHERE email = ? AND contrasena = ?"
+    SQL_SELECT = "SELECT nombre_user, first_name, full_name, email, tipo FROM Usuarios"
+    SQL_CHECK_LOGIN = "SELECT nombre_user, first_name, full_name, email, tipo FROM Usuarios WHERE nombre_user = ? AND contrasena = ?"
 
     def consultarLogin(self, loginVO):
         cursor = self.getCursor()
-
         try:
             cursor.execute(self.SQL_CHECK_LOGIN, (loginVO.nombre, loginVO.contrasena))
-            rows = cursor.fetchone()
-
-            if rows == None:
+            row = cursor.fetchone()
+            if row is None:
                 return None
-            else:
-                nombre, primer_apellido, segundo_apellido, email = rows
-                usuario = UsuariosVO(nombre, primer_apellido, segundo_apellido, email)
-                return usuario
-            
+            nombre_user, first_name, full_name, email, tipo = row
+            return UsuariosVO(nombre_user, first_name, full_name, email, tipo)
         except Exception as e:
             print(e)
+            return None
 
     def select(self):
         cursor = self.getCursor()
         users = []
-
         try:
             cursor.execute(self.SQL_SELECT)
             rows = cursor.fetchall()
-
             for row in rows:
-                nombre, primer_apellido, segundo_apellido, email = row
-                usuario = UsuariosVO(nombre, primer_apellido, segundo_apellido, email)
-
-
+                nombre_user, first_name, full_name, email, tipo = row
+                users.append(UsuariosVO(nombre_user, first_name, full_name, email, tipo))
         except Exception as e:
             print(e)
+        return users
 
         return users
