b7f64a7e77842e569bcde45c55c3c868bb45e287
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-24 22:42:15 +0200

    Update Logica.py

--- src/modelo/Logica.py
+++ src/modelo/Logica.py
@@ -1,16 +1,16 @@
-from src.modelo.dao.UserDaoJDBC import UserDaoJBDC
+from src.modelo.dao.UserDaoJDBC import UserDaoJDBC
 
 class Logica():
     def pruebaSelect(self):
-        userDao = UserDaoJBDC()
+        userDao = UserDaoJDBC()
         usuarios = userDao.select()
         for user in usuarios:
-            print(user.__nombre)
+            print(user.nombre_user)
 
-    def comprobarLogin(self, LoginVO):
-        login_dao = UserDaoJBDC()
-        return login_dao.consultarLogin(LoginVO)
-    
-    def registrarUsuario(self, RegistroVO):
-        registro_dao = UserDaoJBDC()
-        return registro_dao.registrarUsuario(RegistroVO)
+    def comprobarLogin(self, loginVO):                                               
+        login_dao = UserDaoJDBC()                                                    
+        return login_dao.comprobarLogin(loginVO)                                     
+                                                                               
+    def registrarUsuario(self, registroVO):                                          
+        registro_dao = UserDaoJDBC()                                                 
+        return registro_dao.registrarUsuario(registroVO) 
