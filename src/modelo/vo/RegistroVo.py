0e5d6d8e36f41d92b16d909fc1ae26ac1f23480d
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-24 22:57:13 +0200

    Update RegistroVo.py

--- src/modelo/vo/RegistroVo.py
+++ src/modelo/vo/RegistroVo.py
@@ -1,14 +1,15 @@
-def __init__(self, nombre, apellidos, correo, contrasena):                              
-    self._nombre = nombre                                                               
-    self._apellidos = apellidos                                                         
-    self._correo = correo                                                               
-    self._contrasena = contrasena
-    
-@property                                                                               
-def nombre(self): return self._nombre                                                   
-@property                                                                               
-def apellidos(self): return self._apellidos                                             
-@property                                                                               
-def correo(self): return self._correo                                                   
-@property                                                                               
-def contrasena(self): return self._contrasena
+class RegistroVO:
+    def __init__(self, nombre, apellidos, correo, contrasena):                              
+        self._nombre = nombre                                                               
+        self._apellidos = apellidos                                                         
+        self._correo = correo                                                               
+        self._contrasena = contrasena
+            
+    @property                                                                               
+    def nombre(self): return self._nombre                                                   
+    @property                                                                               
+    def apellidos(self): return self._apellidos                                             
+    @property                                                                               
+    def correo(self): return self._correo                                                   
+    @property                                                                               
+    def contrasena(self): return self._contrasena
