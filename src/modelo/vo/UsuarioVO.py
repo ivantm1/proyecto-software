4a25d98b7f68be2a72bdaaf3fd88165fc803b777
Ivan <itejam00@estudiantes.unileon.es>
2026-04-26 00:03:48 +0200

    Crear UsuarioVO

---
+++ src/modelo/vo/UsuarioVO.py
@@ -0,0 +1,22 @@
+class UsuarioVO:
+    def __init__(self, nombre, apellidos, correo, contrasena):
+        self._nombre = nombre
+        self._apellidos = apellidos
+        self._correo = correo
+        self._contrasena = contrasena
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def apellidos(self):
+        return self._apellidos
+    
+    @property
+    def correo(self):
+        return self._correo
+    
+    @property
+    def contrasena(self):
+        return self._contrasena
