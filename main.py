fbd417dec8be0af4b1d7fed8e6a3944e373f3779
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-26 16:55:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
 from src.vista.Login import Login
 from src.vista.Registro import Registro
 from src.vista.Estudiante import Estudiante
 from src.vista.Bibliotecario import Bibliotecario
-from src.modelo.Logica import Logica
-from src.controlador.ControladorPrincipal import ControladorPrincipal
 from src.vista.Catalogo import Catalogo
 from src.vista.MisPrestamos import MisPrestamos
 from src.vista.Perfil import Perfil
+from src.vista.Prestamo import Prestamo
 from src.vista.Sanciones import Sanciones
 from src.vista.Devolucion import Devolucion
+from src.modelo.Logica import Logica
+from src.controlador.ControladorPrincipal import ControladorPrincipal
 
-import os.path
-os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     app = QApplication([])
-    login = Login()
-    registro = Registro()
-    modelo = Logica()
-    estudiante= Estudiante()
-    bibliotecario= Bibliotecario()
+
+    login         = Login()
+    registro      = Registro()
+    estudiante    = Estudiante()
+    bibliotecario = Bibliotecario()
     catalogo      = Catalogo()
     mis_prestamos = MisPrestamos()
     perfil        = Perfil()
+    prestamo      = Prestamo()
     sanciones     = Sanciones()
     devolucion    = Devolucion()
+    modelo        = Logica()
 
     controlador = ControladorPrincipal(
-            modelo, login, registro, estudiante, bibliotecario,
-            catalogo, mis_prestamos, perfil, sanciones, devolucion
-        )
+        modelo, login, registro, estudiante, bibliotecario,
+        catalogo, mis_prestamos, perfil, prestamo, sanciones, devolucion
+    )
 
     login.controlador = controlador
     controlador.ventanaIniciarSesion()
