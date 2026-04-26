c7baac4d86c786c2e445a19fcdc51528c641c26b
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-26 16:58:24 +0200

    Update Logica.py

--- src/modelo/Logica.py
+++ src/modelo/Logica.py
@@ -1,4 +1,9 @@
 from src.modelo.dao.UserDaoJDBC import UserDaoJDBC
+from src.modelo.Logica import Logica
+from src.modelo.dao.LibroDaoJDBC import LibroDaoJDBC
+from src.modelo.dao.PrestamoDaoJDBC import PrestamoDaoJDBC
+from src.modelo.dao.ReservaDaoJDBC import ReservaDaoJDBC
+from src.modelo.dao.SancionDaoJDBC import SancionDaoJDBC
 
 class Logica():
     def pruebaSelect(self):
@@ -14,3 +19,73 @@ class Logica():
     def registrarUsuario(self, registroVO):                                          
         registro_dao = UserDaoJDBC()                                                 
         return registro_dao.registrarUsuario(registroVO) 
+
+    def obtenerCatalogo(self):
+        return LibroDaoJDBC().obtenerCatalogo()
+
+    def buscarPorTitulo(self, titulo):
+        return LibroDaoJDBC().buscarPorTitulo(titulo)
+
+    def buscarPorTema(self, tema):
+        return LibroDaoJDBC().buscarPorTema(tema)
+
+    def altaLibro(self, libroVO):
+        return LibroDaoJDBC().altaLibro(libroVO)
+
+    def bajaLibro(self, isbn):
+        return LibroDaoJDBC().bajaLibro(isbn)
+
+    def obtenerReservados(self):
+        return LibroDaoJDBC().obtenerReservados()
+
+    def buscarPorISBN(self, isbn):
+        return LibroDaoJDBC().buscarPorISBN(isbn)
+    
+    def registrarPrestamo(self, isbn, correo_estudiante):
+        return PrestamoDaoJDBC().registrarPrestamo(isbn, correo_estudiante)
+
+    def registrarDevolucion(self, isbn):
+        return PrestamoDaoJDBC().registrarDevolucion(isbn)
+
+    def obtenerPrestamosEstudiante(self, correo_estudiante):
+        return PrestamoDaoJDBC().obtenerPrestamosEstudiante(correo_estudiante)
+
+    def obtenerPrestamosProximos(self, correo_estudiante):
+        return PrestamoDaoJDBC().obtenerPrestamosProximos(correo_estudiante)
+
+    def prorrogarPrestamo(self, isbn):
+        return PrestamoDaoJDBC().prorrogarPrestamo(isbn)
+
+    def contarPrestamosEstudiante(self, correo_estudiante):
+        return PrestamoDaoJDBC().contarPrestamosEstudiante(correo_estudiante)
+
+    def aplicarSancionRetraso(self, correo_estudiante, semanas_retraso):
+        return SancionDaoJDBC().aplicarSancionRetraso(correo_estudiante, semanas_retraso)
+
+    def aplicarSancionDanio(self, correo_estudiante, semanas_sancion=3):
+        return SancionDaoJDBC().aplicarSancionDanio(correo_estudiante, semanas_sancion)
+
+    def obtenerSancionesEstudiante(self, correo_estudiante):
+        return SancionDaoJDBC().obtenerSancionesEstudiante(correo_estudiante)
+
+    def tieneSancionActiva(self, correo_estudiante):
+        return SancionDaoJDBC().tieneSancionActiva(correo_estudiante)
+
+    def crearReserva(self, isbn, correo_estudiante):
+        return ReservaDaoJDBC().crearReserva(isbn, correo_estudiante)
+
+    def cancelarReserva(self, isbn):
+        return ReservaDaoJDBC().cancelarReserva(isbn)
+
+    def obtenerReservasEstudiante(self, correo_estudiante):
+        return ReservaDaoJDBC().obtenerReservasEstudiante(correo_estudiante)
+
+    def contarReservasEstudiante(self, correo_estudiante):
+        return ReservaDaoJDBC().contarReservasEstudiante(correo_estudiante)
+
+    def reservaExpirada(self, isbn):
+        return ReservaDaoJDBC().reservaExpirada(isbn)
+
+    def obtenerReservaPorLibro(self, isbn):
+        return ReservaDaoJDBC().obtenerReservaPorLibro(isbn)
+
