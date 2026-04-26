8d98b36e0a16b227892e1273a48aa8a94b720c97
PabloMerinoGarrido <pmerig01@estudiantes.unileon.es>
2026-04-26 16:39:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from src.vista.Estudiante import Estudiante
 from src.vista.Bibliotecario import Bibliotecario
 from src.modelo.Logica import Logica
 from src.controlador.ControladorPrincipal import ControladorPrincipal
+from src.vista.Catalogo import Catalogo
+from src.vista.MisPrestamos import MisPrestamos
+from src.vista.Perfil import Perfil
+from src.vista.Sanciones import Sanciones
+from src.vista.Devolucion import Devolucion
 
 import os.path
 os.path.dirname(os.path.abspath(__file__))
@@ -16,8 +21,16 @@ if __name__ == "__main__":
     modelo = Logica()
     estudiante= Estudiante()
     bibliotecario= Bibliotecario()
+    catalogo      = Catalogo()
+    mis_prestamos = MisPrestamos()
+    perfil        = Perfil()
+    sanciones     = Sanciones()
+    devolucion    = Devolucion()
 
-    controlador = ControladorPrincipal(modelo, login, registro, estudiante, bibliotecario)
+    controlador = ControladorPrincipal(
+            modelo, login, registro, estudiante, bibliotecario,
+            catalogo, mis_prestamos, perfil, sanciones, devolucion
+        )
 
     login.controlador = controlador
     controlador.ventanaIniciarSesion()
